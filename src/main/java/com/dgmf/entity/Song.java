09ea68ba514f860a240ac08b18e115b4a39f452a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-21 12:37:42 +0200

    Creating a Song class

---
+++ src/main/java/com/dgmf/entity/Song.java
@@ -0,0 +1,28 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "tbl_song")
+public class Song {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private double duration;
+    @Column(name = "date_created", nullable = false)
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @Column(name = "last_updated")
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
